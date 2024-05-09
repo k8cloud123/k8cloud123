eed5c9342dd7a4761e467d39b73ae71464d4d44a
root <root@AP-5CD413GVGG>
2024-05-09 18:39:02 +0100

    update_config

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,1 +1,11 @@
-ec2_instances.tf
+resource "aws_instance" "app02" {
+    ami = "ami-053a617c6207ecc7b"
+    instance_type = var.instance_type
+    key_name = aws_key_pair.ssh_key.key_name
+    security_groups = [aws_security_group.instance_eu.name]
+    subnet_id = aws_subnet.main.id
+    iam_instance_profile {
+        name = "aws-elasticbeanstalk-ec2-role"
+    }
+
+}
--- ssh_key_pair.tf
+++ ssh_key_pair.tf
@@ -1,1 +1,4 @@
-
+resource "aws_key_pair" "ssh_key" {
+  key_name = var.ssh_key_name
+  public_key = file("~/.ssh/id_rsa.pub")
+}
